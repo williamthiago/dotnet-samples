e7c54ecfd1c3104543c307487dcf3dd090e89d9b
William Thiago <williamthiago@gmail.com>
2016-05-29 01:49:47 -0300

    Add console portable app

---
+++ console-portable-application/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsolePortableApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Sistema operacional: ");
+            Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.OSDescription);
+            Console.WriteLine("Exemplo de Portable App.");
+        }
+    }
+}
